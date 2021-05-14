3bbab2ad6aca49a2d9e1b8e6d2a40bda05e98636
BhCh7051 <bharatchandwani1@gmail.com>
2021-05-14 19:55:04 +0530

    rename

---
+++ chatty/src/services/firebase.js
@@ -0,0 +1,20 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/database";
+require('dotenv').config()
+
+firebaseConfig = {
+ 	apiKey: process.env.REACT_APP_APIKEY,
+ 	authDomain: process.env.REACT_APP_AUTHDOMAIN,
+ 	databaseURL: process.env.REACT_APP_DATABASEURL,
+ 	projectId: process.env.REACT_APP_PROJECTID,
+ 	storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+ 	messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+ 	appId: process.env.REACT_APP_APPID,
+ 	measurementId: process.env.REACT_APP_MEASUREMENTID,
+ };
+
+firebase.initializeApp(firebaseConfig);
+
+export const auth = firebase.auth;
+export const db = firebase.database();
